18ddcc8205f4bee7f9e5a0d554fb884f8a0b5434
unknown <kmcomptech@gmail.com>
2022-10-25 10:09:09 -0500

    Changed config

--- config.py
+++
@@ -1,7 +0,0 @@
-from azure.cosmos import exceptions, CosmosClient, PartitionKey
-
-url = 'https://kmazureresume.documents.azure.com:443/'
-key = '1Cma5jR9rzgmQnuSiikrJq88qrGBrIGgosrQU1hxngbDMpFl973XQVUEhpGXSAeGgkpE9g5EQUorfggW7eH1UA=='
-client = CosmosClient(url, key)
-database_name = 'AzureResume'
-container_name = 'api'
